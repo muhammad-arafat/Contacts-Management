8e928b097905e5c2d6f11c3993f6701562236023
M-Arafat61 <arafjust16@gmail.com>
2024-04-18 23:41:21 +0600

    contact model creation with optional email field

---
+++ backend/models/contact.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ContactInfo = mongoose.model("ContactInfo", contactSchema);
+export default ContactInfo;
